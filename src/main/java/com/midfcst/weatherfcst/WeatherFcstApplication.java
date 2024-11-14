50183f3bad051b8832d5f5f5f70ce739b9d6ddac
tangpoo <lhinav3@gmail.com>
2024-11-14 15:23:30 +0900

    git init

---
+++ src/main/java/com/midfcst/weatherfcst/WeatherFcstApplication.java
@@ -0,0 +1,13 @@
+package com.midfcst.weatherfcst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherFcstApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherFcstApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/midfcst/weatherfcst/WeatherFcstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.midfcst.weatherfcst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherFcstApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
