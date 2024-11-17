f6ccc0a9f10141bb03ff0fb0b22534972067455c
tangpoo <lhinav3@gmail.com>
2024-11-17 22:46:32 +0900

    API 별 Response 객체 생성

---
+++ web/src/main/java/com/weather/weatherfcst/dto/ShortNcstRes.java
@@ -0,0 +1,16 @@
+package com.weather.weatherfcst.dto;
+
+public class ShortNcstRes {
+    private String resultCode;
+    private String resultMsg;
+    private String numOfRows;
+    private String pageNo;
+    private String totalCount;
+    private String dataType;
+    private String baseDate;
+    private String baseTime;
+    private String nx;
+    private String ny;
+    private String category;
+    private String obsrValue;
+}
---
+++ web/src/main/java/com/weather/weatherfcst/dto/WthrWrnRes.java
@@ -0,0 +1,14 @@
+package com.weather.weatherfcst.dto;
+
+public class WthrWrnRes {
+    private String resultCode;
+    private String resultMsg;
+    private String numOfRows;
+    private String pageNo;
+    private String totalCount;
+    private String dataType;
+    private String title;
+    private String stnId;
+    private String tmSeq;
+    private String tmFc;
+}
